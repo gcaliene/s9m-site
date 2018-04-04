adbbc0361a046869bd394a6167f083aed14701f6
Shenole Latimer <shenole@gmail.com>
2018-04-04 10:20:24 -0400

    Functionality of form that displays file upload requirements, based on template selection, is complete. Need to start connecting form elements to Firebase

--- js/adendum.js
+++ js/adendum.js
@@ -4,23 +4,47 @@
 const needsList = document.querySelector('#require');
 const templateList = document.querySelector('#templates');
 
-// Check value of template selection dropdown then open requirement window
+// Check value of template selection dropdown then display appropriate requirements
 function requirements() {
   // Chosen template is displayed
   needsList.innerHTML = `The ${templateList.value} template requires the following file uploads:`;
 
-
-
-
-
   // Array of requirement items
-  let items = ['Your artist/band biography. These are the accepted file formats: .doc, .docx, .rtf, .text, .txt', 'One image for your homepage. This image should idealy be a high quality image that is 300dpi.', 'One image to be used on the Biography Page of your site.', 'Up to eight images that you would like placed in your gallery.', 'One image that you would like to have featured in the Live Events section of the Moderato Template', 'One image that you would like featured in the circular image space in the Biography section of the Power Chord template', 'One image that you would like to have featured in the Contact section of the Power Chord template', 'An image for each member of your band. Make sure to provide their name and instrument. Up to six band members may be included.', 'Direct links to where your music can be purchased online', 'Direct links to online samples of your music (ie. SoundCloud, etc.)', 'Direct links to any video you may have on Youtube or Vimeo.'];
+  let items = [];
+
+  items[0] = 'Your artist/band biography. These are the accepted file formats: .doc, .docx, .rtf, .text, .txt';
+  items[1] = 'One image for your homepage. This image should idealy be a high quality image that is 300dpi.';
+  items[2] = 'One image to be used on the Biography Page of your site.';
+  items[3] = 'Up to eight images that you would like placed in your gallery.';
+  items[4] = 'One image that you would like to have featured in the Live Events section of the Moderato Template';
+  items[5] = 'One image that you would like featured in the circular image space in the Biography section of the Power Chord template';
+  items[6] = 'One image that you would like to have featured in the Contact section of the Power Chord template';
+  items[7] = 'An image for each member of your band. Make sure to provide their name and instrument. Up to six band members may be included.';
+  items[8] = 'Direct links to where your music can be purchased online';
+  items[9] = 'Direct links to online samples of your music (ie. SoundCloud, etc.)';
+  items[10] = 'Direct links to any video you may have on Youtube or Vimeo.';
 
   // Appropriate lists are displayed based on template selection
-  if(templateList.value == 'Augmented Fifth') {
-    items.splice(4, 4);
-    populateList();
-  }
+  switch (templateList.value) {
+    case 'Augmented Fifth':
+      items.splice(4, 4);
+      populateList();
+      break;
+    case 'Dorian Minor':
+      items.splice(4, 4);
+      populateList();
+      break;
+    case 'Moderato':
+      populateList();
+      break;
+    case 'Power Chord':
+      items.splice(7, 1);
+      populateList();
+      break;
+    default:
+      needsList.innerHTML = '';
+      break;
+  };
 
   // Function to populate requirement list
   function populateList() {
