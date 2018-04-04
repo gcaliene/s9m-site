e5b88ee0f6fb433c72058f683411f10101e8b577
Shenole Latimer <shenole@gmail.com>
2018-04-03 20:04:43 -0400

    Working on file requirement window on form.

--- js/adendum.js
+++ js/adendum.js
@@ -1,3 +1,23 @@
 ///////////////////////////////
 // TEMPLATE UPLOAD REQUIREMENTS
 ///////////////////////////////
+const needsList = document.querySelector('#require');
+const templateList = document.querySelector('#templates');
+
+// Check value of template selection dropdown then open requirement window
+function requirements() {
+  needsList.innerHTML = `The ${templateList.value} template requires the following uploads for you to make:`;
+
+
+
+
+
+
+
+
+
+
+  let newLi = document.createElement('li');
+  needsList.appendChild(newLi);
+  newLi.innerHTML = '6 pictures';
+}
